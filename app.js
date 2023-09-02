70fb8597f4769b151d515e6b9c9b13419939335f
MarcNettles <70242488+MarcNettles@users.noreply.github.com>
2023-09-02 00:29:42 -0600

    Removed inline javascript from tictactoe

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.use(helmet({
             contentSecurityPolicy: {
                 directives: {
                     "script-src": ["'self'", 
-                                    "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js",
+                                    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js", //includes Popper
+                                    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js",
                                     "https://code.jquery.com/jquery-3.3.1.min.js",
                                     "https://cdn.jsdelivr.net/npm/popper.min.js",
                                     "https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Nunito+Sans:opsz@6..12&family=Poppins:wght@300&family=Roboto&display=swap"
