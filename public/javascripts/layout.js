52aad3492a1f0050e3efa75c0772cbe25617ba88
MarcNettles <70242488+MarcNettles@users.noreply.github.com>
2023-09-06 10:58:10 -0600

    Having issues with Skip to Main Content on NVDA
    Skip to Main Content link gets skipped over on first tab, but only with a screen reader active.

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -10,14 +10,29 @@ document.addEventListener('DOMContentLoaded', function (){
     //--------START-------------/
     var skipLink = document.getElementById("skip_link");
 
-    skipLink.addEventListener("focus", function (e){
+    // Added this so that when the user
+    var firstNavLink = document.getElementById("firstNavLink");
+
+    skipLink.addEventListener("focus", function (e){ // Listen for it to be focused (e.g. when we tab to it), then remove the sr-only stuff so it becomes visible.
 
         skipLink.classList.remove("sr-only");
         skipLink.classList.remove("sr-only-focusable");
+        
+
+        document.addEventListener("keyup", function (e){
+            if(e.key==="Enter"){
+                firstNavLink.focus();
+            }
+        });
+
+        
+        
 
     });
-    skipLink.addEventListener("blur", function (e){
+    skipLink.addEventListener("blur", function (e){ // Listen for it to be unfocused, then put the sr-only stuff back on.
         skipLink.classList.add("sr-only");
         skipLink.classList.add("sr-only-focusable");
     });
+
+    
 });
